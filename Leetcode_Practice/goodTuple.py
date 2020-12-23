c02b05e75c8f7f2b8fc271b921fc46333ce412a1
Urvashi Singh <hanu@Urvashis-MacBook-Pro.local>
2020-12-23 13:37:46 -0800

    Leetcode Folder Created

---
+++ Leetcode_Practice/goodTuple.py
@@ -0,0 +1,24 @@
+<<<<<<< HEAD
+def f(a):
+    count = 0
+    for i in range(1, len(a)-1):
+        if a[i-1] == a[i] == a[i+1]:
+            continue
+        elif a[i-1] == a[i] or a[i] == a[i+1] or a[i-1] == a[i+1]:
+            count += 1
+    print(count)
+
+a = [1,1,2,1,2,1,1]
+=======
+def f(a):
+    count = 0
+    for i in range(1, len(a)-1):
+        if a[i-1] == a[i] == a[i+1]:
+            continue
+        elif a[i-1] == a[i] or a[i] == a[i+1] or a[i-1] == a[i+1]:
+            count += 1
+    print(count)
+
+a = [1,1,2,1,2,1,1]
+>>>>>>> 6a511d70a9044aa21ff457120200837581634c3b
+f(a)
